eb94190e5a119df5f09700e31625ca79080d1cda
YifanLiang-hust <yfl@hust.edu.cn>
2025-05-31 17:08:20 +0800

    第二次提交

--- script/DPM/b16_ep20.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=2,3 python DPM_main.py --config-file ./config/trainer/ImageNet_b16_DPM.yaml --model_path ./checkpoint/ViT-B-16.pt --output_dir ./output/DPM/loss2+3/b16_ep20 --train --loss2 1 --loss3 1
-CUDA_VISIBLE_DEVICES=2,3 python DPM_main.py --config-file ./config/trainer/ImageNet_b16_DPM.yaml --model_path ./checkpoint/ViT-B-16.pt --output_dir ./output/DPM/loss2/b16_ep20 --train --loss2 1 --loss3 0
-CUDA_VISIBLE_DEVICES=2,3 python DPM_main.py --config-file ./config/trainer/ImageNet_b16_DPM.yaml --model_path ./checkpoint/ViT-B-16.pt --output_dir ./output/DPM/loss3/b16_ep20 --train --loss2 0 --loss3 1
